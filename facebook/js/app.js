9ae4ecb043dacf5017b8d7a127636eec341dc2be
Sergio Luevano <sergioluevano@MacBook-Pro-de-Sergio.local>
2015-10-15 16:17:22 -0500

    first upload

---
+++ facebook/js/app.js
@@ -0,0 +1,20 @@
+var app=angular.module('FacebookApp',[]);
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1507306549584583',
+      xfbml      : true,
+      version    : 'v2.5',
+      cookie: true,
+      status: true,
+      xfbml: true
+    });
+    watchLoginChange();
+  };
+  
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
---
+++ facebook/js/controlador.js
@@ -0,0 +1,140 @@
+app.controller('facebookController',function($scope,$rootScope){
+	gobernadores=["MiguelMarquezM", 
+	"eruviel",
+	"MiguelAngelMancera",
+	'JaimeRodriguezElBronco',
+	'franciscokikovega',
+	'manuelvelascoc',
+	'CarrerasGobernador',
+	'SilvanoAureoles',
+	'panchdominguez',
+	'AlejandroMC',
+	'ClaudiaPavlovich',
+	'arturonunez',
+	'mariolopezvaldez',
+	'GobiernodeAguascalientes',
+	'robertosandovalc',
+	'PacoOlveraFanPage',
+	'miguelzac',
+	'jaristoteles.sandoval',
+	'jherreracaldera',
+	'RafaelMorenoValle',
+	'GabinoCueMonteagudo',
+	'betoborgefans',
+	'gracoramirezg',
+	'cesarduartejaquez',
+	'mario.anguiano.gobernador',
+	'egidiotorrecantu'
+	];
+	gobiernos=["MiguelMarquezM", 
+	"eruviel",
+	"MiguelAngelMancera",
+	'JaimeRodriguezElBronco',
+	'franciscokikovega',
+	'manuelvelascoc',
+	'CarrerasGobernador',
+	'SilvanoAureoles',
+	'panchdominguez',
+	'AlejandroMC',
+	'ClaudiaPavlovich',
+	'arturonunez',
+	'mariolopezvaldez',
+	'GobiernodeAguascalientes',
+	'robertosandovalc',
+	'PacoOlveraFanPage',
+	'miguelzac',
+	'jaristoteles.sandoval',
+	'jherreracaldera',
+	'RafaelMorenoValle',
+	'GabinoCueMonteagudo',
+	'betoborgefans',
+	'gracoramirezg',
+	'cesarduartejaquez',
+	'mario.anguiano.gobernador',
+	'egidiotorrecantu'
+	];
+	$scope.result=[];
+	$scope.resultGob=[];
+	$rootScope.logged=false;
+	
+	$scope.getMe=function(){
+		FB.api('/me?fields=likes',function(response){ 
+					$rootScope.$apply(function() { 
+      					//$rootScope.user = _self.user = res; 
+      					console.log(response);
+    				});			
+		});
+	}
+
+	$scope.getLikes=function(){
+		var res=[];
+		angular.forEach(gobernadores, function(i){
+	     		var obj={};
+				FB.api('/' + i + '?fields=likes,picture',function(response){ 
+					$rootScope.$apply(function() { 
+						obj["likes"] = response.likes;
+						obj["foto"] = response.picture;
+						obj["nombre"] = i;
+						$scope.result.push(obj);
+					});
+				});
+			});
+	};
+
+	$scope.getLikesGobiernos=function(){
+		var res=[];
+		angular.forEach(gobiernos, function(i){
+	     		var obj={};
+				FB.api('/' + i + '?fields=likes',function(response){ 
+					$rootScope.$apply(function() { 
+						obj["likes"] = response.likes;
+						obj["nombre"] = i;
+						$scope.resultGob.push(obj);
+					});
+				});
+			});
+	};
+	
+
+	watchLoginChange = function() {
+		console.log('working...');
+	  	FB.Event.subscribe('auth.statusChange', function(res) {
+		    if (res.status === 'connected') {
+		    	$rootScope.logged=true;
+		    	$scope.getLikes();
+		    	//$scope.getLikesGobiernos();
+		    } 
+		    else {
+		      $rootScope.logged=false;
+		    }
+	  });
+	}
+	
+
+	$scope.FBLogin=function(){
+		FB.login(function(response) {
+	    if (response.authResponse) {
+	    	$scope.logged=true;
+	    	var accesToken=FB.getAuthResponse();	
+		 
+	    } else {
+	     console.log('User cancelled login or did not fully authorize.');
+	    }
+		});
+	}
+
+	$scope.FBLogot=function(){
+		
+		FB.logout(function(response) {
+			 $rootScope.$apply(function() { 
+  				 $rootScope.logged=false;
+				$scope.result=[];
+  			});
+		});
+	}
+
+	
+
+	
+
+});
